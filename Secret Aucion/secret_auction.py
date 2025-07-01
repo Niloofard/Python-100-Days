b036ab8a06f98dab76064de684f44270dd08adc4
niloofar <niloofar.dehghani@mail.concordia.ca>
2025-07-01 12:21:24 -0400

    comment

--- Secret Aucion/secret_auction.py
+++ Secret Aucion/secret_auction.py
@@ -6,7 +6,7 @@ while more_bids== "yes":
     bid_amount[name] = int(input("What is your bid? "))
     more_bids= input("Are There any Other Bidders? Type Yes or No: ").lower()
     print("\n" * 100)
-
+#find max
 max_bid= 0
 for name in bid_amount:
     if bid_amount[name] > max_bid:
