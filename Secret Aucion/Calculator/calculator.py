7e0ca0f20ef93ebdc727131a48d744c1abac6790
niloofar <niloofar.dehghani@mail.concordia.ca>
2025-07-02 11:26:22 -0400

    calculator with dictionary

--- Secret Aucion/Calculator/calculator.py
+++ Secret Aucion/Calculator/calculator.py
@@ -7,57 +7,26 @@ def multiply(n1,n2):
 
 def divide(n1,n2):
     return n1/n2
-n1=0
-is_set_n1=False
-while 1:
-    reset= True
-    while reset == True:
-        if is_set_n1 == False:
-            n1= int(input ("What is the first number?"))
-        op= input ("pich an Operation? \n 1. +\n 2. -\n 3. *\n 4. /\n")
-        n2=int(input ("What is the second number?"))
-        if op== "+":
-            result=add(n1,n2)
-            print(result)
-            dec = input(f"Type y to continue with {result} or Type n to start a new calculation:").lower()
-            if dec== "n":
-                reset= False
-            elif dec== "y":
-                n1=result
-                is_set_n1=True
-                reset = False
 
-        if op== "-":
-            result=subtract(n1,n2)
-            print(result)
-            dec = input(f"Type y to continue with {result} or Type n to start a new calculation:").lower()
-            if dec== "n":
-                reset= False
-            elif dec== "y":
-                n1=result
-                is_set_n1=True
-                reset = False
-        if op== "/":
-            result=divide(n1,n2)
-            print(result)
-            dec = input(f"Type y to continue with {result} or Type n to start a new calculation:").lower()
-            if dec== "n":
-                reset= False
-            elif dec== "y":
-                n1=result
-                is_set_n1=True
-                reset = False
-        if op== "*":
-            result=multiply(n1,n2)
-            print(result)
-            dec = input(f"Type y to continue with {result} or Type n to start a new calculation:").lower()
-            if dec== "n":
-                reset= False
-            elif dec== "y":
-                n1=result
-                is_set_n1=True
-                reset = False
+operations = {"+":add,"-":subtract,"*":multiply,"/":divide}
 
+def calculator():
+    num_1 = float(input("Enter the first number: "))
+    comulative_flag = True
+    while comulative_flag:
+        for symbol in operations:
+            print(symbol,"\n")
+        chosed_op= input("Please choose the operation you want to:")
+        num_2 = float(input("Enter the second number: "))
+        result = operations[chosed_op](num_1,num_2)
+        print(f"{num_1} + {num_2} = {result}")
+        choice = input(f"Type 'y' if you want to continue with {result} or 'n' if you want to reset the calculator: ").lower()
+        if choice == "y":
+            num_1 = result
+        else:
+            print("\n" * 20)
+            calculator()
+calculator()
 
 
 
